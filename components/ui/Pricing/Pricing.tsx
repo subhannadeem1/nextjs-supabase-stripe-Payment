7d98330586f817ea7414ac9329c4d7bdfdc19110
subhan1212 <70110442@student.uol.edu.pk>
2025-01-09 17:43:42 +0500

    first commit

---
+++ components/ui/Pricing/Pricing.tsx
@@ -0,0 +1,198 @@
+'use client';
+
+import { useEffect } from 'react';
+import Button from '@/components/ui/Button';
+import LogoCloud from '@/components/ui/LogoCloud';
+import type { Tables } from '@/types_db';
+import { getStripe } from '@/utils/stripe/client';
+import { checkoutWithStripe } from '@/utils/stripe/server';
+import { getErrorRedirect } from '@/utils/helpers';
+import { User } from '@supabase/supabase-js';
+import cn from 'classnames';
+import { useRouter, usePathname } from 'next/navigation';
+import { useState } from 'react';
+
+type Subscription = Tables<'subscriptions'>;
+type Product = Tables<'products'>;
+type Price = Tables<'prices'>;
+interface ProductWithPrices extends Product {
+  prices: Price[];
+}
+interface PriceWithProduct extends Price {
+  products: Product | null;
+}
+interface SubscriptionWithProduct extends Subscription {
+  prices: PriceWithProduct | null;
+}
+
+interface Props {
+  user: User | null | undefined;
+  products: ProductWithPrices[];
+  subscription: SubscriptionWithProduct | null;
+}
+
+type BillingInterval = 'lifetime' | 'year' | 'month';
+
+export default function Pricing({ user, products, subscription }: Props) {
+  const intervals = Array.from(
+    new Set(
+      products.flatMap((product) =>
+        product?.prices?.map((price) => price?.interval)
+      )
+    )
+  );
+  const router = useRouter();
+  const [billingInterval, setBillingInterval] =
+    useState<BillingInterval>('month');
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
+  const currentPath = usePathname();
+
+  const handleStripeCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id);
+
+    if (!user) {
+      setPriceIdLoading(undefined);
+      return router.push('/signin/signup');
+    }
+
+    const { errorRedirect, sessionId } = await checkoutWithStripe(
+      price,
+      currentPath
+    );
+
+    if (errorRedirect) {
+      setPriceIdLoading(undefined);
+      return router.push(errorRedirect);
+    }
+
+    if (!sessionId) {
+      setPriceIdLoading(undefined);
+      return router.push(
+        getErrorRedirect(
+          currentPath,
+          'An unknown error occurred.',
+          'Please try again later or contact a system administrator.'
+        )
+      );
+    }
+
+    const stripe = await getStripe();
+    stripe?.redirectToCheckout({ sessionId });
+
+    setPriceIdLoading(undefined);
+  };
+
+  // Dynamically add the Stripe Pricing Table script
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://js.stripe.com/v3/pricing-table.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <section className="bg-black">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        {/* Existing Pricing Plans */}
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Pricing Plans
+          </h1>
+          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+            Start building for free, then add a site plan to go live. Account
+            plans unlock additional features.
+          </p>
+          <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
+            {intervals.includes('month') && (
+              <button
+                onClick={() => setBillingInterval('month')}
+                type="button"
+                className={`${
+                  billingInterval === 'month'
+                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
+                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap`}
+              >
+                Monthly billing
+              </button>
+            )}
+            {intervals.includes('year') && (
+              <button
+                onClick={() => setBillingInterval('year')}
+                type="button"
+                className={`${
+                  billingInterval === 'year'
+                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
+                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap`}
+              >
+                Yearly billing
+              </button>
+            )}
+          </div>
+        </div>
+
+        {/* Custom Pricing Cards */}
+        <div className="mt-12 flex flex-wrap justify-center gap-6">
+          {products.map((product) => {
+            const price = product?.prices?.find(
+              (price) => price.interval === billingInterval
+            );
+            if (!price) return null;
+            const priceString = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: price.currency!,
+              minimumFractionDigits: 0,
+            }).format((price?.unit_amount || 0) / 100);
+            return (
+              <div
+                key={product.id}
+                className="flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900"
+              >
+                <div className="p-6">
+                  <h2 className="text-2xl font-semibold text-white">
+                    {product.name}
+                  </h2>
+                  <p className="mt-4 text-zinc-300">{product.description}</p>
+                  <p className="mt-8">
+                    <span className="text-5xl font-extrabold white">
+                      {priceString}
+                    </span>
+                    <span className="text-base font-medium text-zinc-100">
+                      /{billingInterval}
+                    </span>
+                  </p>
+                  <Button
+                    variant="slim"
+                    type="button"
+                    loading={priceIdLoading === price.id}
+                    onClick={() => handleStripeCheckout(price)}
+                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md"
+                  >
+                    {subscription ? 'Manage' : 'Subscribe'}
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* Stripe Pricing Table */}
+        <div className="mt-12">
+          <h2 className="text-2xl font-semibold text-white text-center">
+            Stripe Pricing Table
+          </h2>
+          <stripe-pricing-table
+            pricing-table-id="prctbl_1QecHyBiDW7kGn9FPxuXnYSG"
+            publishable-key="pk_test_51QeWvBBiDW7kGn9FXnrQsuQ46773FDQ6AieGbM0Fz7nuI7HrQRTsNteEyzdrrBxnUrO3pfTrznzWBRKZIFXW4sNk008Z2gsRuJ"
+          ></stripe-pricing-table>
+        </div>
+      </div>
+      <LogoCloud />
+    </section>
+  );
+}
